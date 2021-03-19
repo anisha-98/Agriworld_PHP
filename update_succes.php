9a2ca4f5ce5405f489fb47d7938b16849ca51692
anisha-98 <anishanjan2019@gmail.com>
2021-03-20 00:27:20 +0530

    first commit

---
+++ update_succes.php
@@ -0,0 +1,65 @@
+<!--
+Author: Colorlib
+Author URL: https://colorlib.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Creative Colorlib SignUp Form</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<link href="css/styleform.css" rel="stylesheet" type="text/css" media="all" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+<!-- //Custom Theme files -->
+<!-- web font -->
+<link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+<!-- //web font -->
+</head>
+
+<body>
+	<a href="seller_update.php" class=" btn btn-outline-primary"> < previous </a>
+	<!-- main -->
+	<div class="main-w3layouts wrapper">
+		<h1>UPDATED</h1>
+		<div class="main-agileinfo">
+			<div class="agileits-top">
+				
+				<form action="#" method="post">
+				
+				<?php
+					echo "SUCCESFULLY UPDATED";
+				?>
+					
+						</label>
+						<div class="clear"> </div>
+					</div>
+					
+				</form>
+				
+			</div>
+		</div>
+		<!-- copyright -->
+		<div class="colorlibcopy-agile">
+			<p> <a href="https://colorlib.com/" target="_blank"></a></p>
+		</div>
+		<!-- //copyright -->
+		<ul class="colorlib-bubbles">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<!-- //main -->
+</body>
+</html>
